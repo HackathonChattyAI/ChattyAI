00077c73a850cd6d06e37fce06f0fc15c1c907e6
Ilphat <ilphat.gi@gmail.com>
2023-07-26 22:55:35 +0300

    some six

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from "react"
 import LayoutWithHeader from "../layouts/LayoutWithHeader/LayoutWithHeader"
+import InteractionPanel from "../components/InteractionPanel/InteractionPanel"
 
 const Main: FC = () => {
-  return <LayoutWithHeader title="Chatty AI"></LayoutWithHeader>
+  return (
+    <LayoutWithHeader title="Chatty AI">
+      <InteractionPanel />
+    </LayoutWithHeader>
+  )
 }
 
 export default Main
