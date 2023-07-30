8050466490c3361c90657f88701d93abd9aef1d5
Maria Gromyko <mazvereva91@mail.ru>
2023-07-30 12:00:22 +0300

    style fix

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -8,7 +8,7 @@ const NotFound: FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>404</h1>
       <p className={styles.text_content}>Упс, у нас что-то сломалось</p>
-      <ReactLink to={Routes.NOT_FOUND}>Обновить</ReactLink>
+      <ReactLink to={Routes.HOME}>Обновить</ReactLink>
     </div>
   )
 }
