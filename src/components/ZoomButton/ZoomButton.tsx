1ef59c6d15bd3b9da6d1f35936d50f5e58e3fdd3
Maria Gromyko <mazvereva91@mail.ru>
2023-07-30 16:59:51 +0300

    feat: new component ZoomBtn

---
+++ src/components/ZoomButton/ZoomButton.tsx
@@ -0,0 +1,11 @@
+import React, { FC, useEffect, useState } from "react"
+import style from "./ZoomButton.module.scss"
+
+const ZoomButton: FC = () => {
+
+    return (
+        <button className={style.btn_zoom}></button>
+    )
+}
+
+export default ZoomButton
